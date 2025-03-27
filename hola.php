73a66cf3d72865f3ebac6b6b93b7ed592f8e1b31
Javiii25 <javierleiva257@gmail.com>
2025-03-27 14:41:22 -0300

    agregue unos comentarios (linea 2-5) y despues trabajamos con variables(linea6-9 son ejemplos y linea 11-17 es un ejercicio de arreglar)

--- hola.php
+++ hola.php
@@ -1,1 +1,18 @@
-<?php echo "Hola mundo!";?>
+<?php echo "Hola mundo!";
+// Hola //
+/* hola nacho */
+echo "hola yulito";
+#hola diego 
+$nombre = "Manolo"; // Texto, Puede ser con comillas simples o dobles (string) //
+$edad = 31; // Enteros (integer) //
+$altura = 1.80; // Decimales, usando el punto en lugar de la coma (float) //
+$mayorEdad = true; // Verdad o falso (boolean) //
+
+$nombreCompleto = "Juan Perez"; // falta cerrar la linea con un punto y coma //
+$EDAD = 25; 
+$estAtura = 1.75;
+$numero = 10;
+$apellido = "Fernandez"; // no abrio la linea con un $ //
+$pais_de_origen = "Uruguay"; // no abrio la linea con un $, no separo el = y no cerro con un ;//
+$salario = 2000; // el $ que esta al lado del 2000 no iria //
+?>
