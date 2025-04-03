082e8629980c777d17c984ec27558bdfd37142f1
Javiii25 <javierleiva257@gmail.com>
2025-04-03 13:56:46 -0300

    estuvimos trabajando con las diferencias de las comillas simples y dobles, y al final con la concatenacion usando el punto

---
+++ comillas.php
@@ -0,0 +1,14 @@
+<?php echo "Hola mundo!"; 
+$emisora = 'Radio Disney';
+echo "Me gusta escuchar $emisora";
+// las comillas dobles se encargan de fijarse si adentro hay una variable y la muestra como su contenido
+echo "  ";
+echo 'Me gusta escuchar $emisora';
+//las comillas simples muestran todo como texto
+
+$saludo = 'hola';
+$nombre = 'nacho';
+$sn = $saludo . $nombre;
+echo $sn;
+
+?>
