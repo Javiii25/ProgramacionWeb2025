6e5be96771032e215b5cdbcbebf5aa26d25cd462
Javiii25 <javierleiva257@gmail.com>
2025-04-03 14:17:43 -0300

    trabajamos con los operadores de asignacion

---
+++ operadores.php
@@ -0,0 +1,24 @@
+<?php 
+$edad = 1;
+$saludo = 'hola';
+
+$edad += 1;
+echo $edad;
+//el += suma y asigna el valor de la derecha al valor de la variable, basicamente suma 1 al valor de edad
+
+$edad -= 10;
+echo $edad;
+//el -= resta y asigna el valor de la derecha al valor de la variable, basicamente resta 10 al valor de edad
+
+$edad *= 100;
+echo $edad;
+//el *= multiplica y asigna el valor de la derecha al valor de la variable, es decir, multiplica el 1 de edad por el 100
+
+$saludo .= '_nacho';
+echo $saludo;
+// el .= concatena el valor que tiene a la derecha al valor ya existente en saludo
+
+
+
+
+?>
