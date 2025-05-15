577f919d54c8ae5b52ae1bb751b449f465013c52
Javiii25 <javierleiva257@gmail.com>
2025-05-15 15:34:32 -0300

    terminamos de estilizar la actividad, tambien agregamos un array para simplificar

--- convertir2.php
+++ convertir2.php
@@ -2,20 +2,22 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $cantidadEnPesos = floatval($_POST["pesos"]);
 $moneda = ($_POST["moneda"]);
-$valorDolar = 40;
-$valorEuro = 43;
-$valorReal = 8;
+$monedas = [40, 43, 8, 'nacho'];
+//$valorDolar = 40;
+//$valorEuro = 43;
+//$valorReal = 8;
+var_dump($monedas);
 switch ($moneda) {
     case "Dolares":
-        $cantidadTotalDolares = $cantidadEnPesos / $valorDolar;
+        $cantidadTotalDolares = $cantidadEnPesos / $monedas[0];
 echo "Resultado de la conversion: $cantidadEnPesos pesos uruguayos equivalen a " . $cantidadTotalDolares . " dolares";
         break;
     case "Euros":
-        $cantidadTotalEuros = $cantidadEnPesos / $valorEuro;
+        $cantidadTotalEuros = $cantidadEnPesos / $monedas[1];
 echo "Resultado de la conversion: $cantidadEnPesos pesos uruguayos equivalen a " . $cantidadTotalEuros . " euros";
         break;
     case "Reales":
-        $cantidadTotalReales = $cantidadEnPesos / $valorReal;
+        $cantidadTotalReales = $cantidadEnPesos / $monedas[2];
 echo "Resultado de la conversion: $cantidadEnPesos pesos uruguayos equivalen a " . $cantidadTotalReales . " reales";
         break;
 }
