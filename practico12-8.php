69284df6cc2c3b3ae1e43f004b098bdb9cd1e878
Alumno <alumno@isbo.edu.uy>
2025-08-12 17:57:06 -0300

    arrancamos con el practico nuevo que nos dio el profe de arreglos asociativos y html

---
+++ practico12-8.php
@@ -0,0 +1,28 @@
+<?php
+$mascotas = [
+    'Tobi' => [
+        'nombre' => 'Tobi',
+        'edad' => '13',
+        'raza' => 'caniche',
+        'cantVacunas' => '3',
+        'fechaNacimiento' => '10/11/2012',
+        'animal' => 'perro'
+    ],
+    'Techito' => [
+        'nombre' => 'techito',
+        'edad' => '12',
+        'raza' => 'siames',
+        'cantVacunas' => '2',
+        'fechaNacimiento' => '1/1/2015',
+        'animal' => 'gato'
+    ],
+    'Victor' => [
+        'nombre' => 'victor',
+        'edad' => '5',
+        'raza' => 'ruiseñor comun',
+        'cantVacunas' => '1',
+        'fechaNacimiento' => '5/02/2020',
+        'animal' => 'ave'
+    ]
+];
+?>
