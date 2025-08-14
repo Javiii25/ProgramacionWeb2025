99279b36f0204489254c00aac8cf8af7098992b3
Javiii25 <javierleiva257@gmail.com>
2025-08-14 15:40:22 -0300

    empezamos a aprender javascript, nos familiarizamos con las funciones

---
+++ Javascript/practica.js
@@ -0,0 +1,24 @@
+// var nombre = nacho;
+// let edad = 22;
+// const PI = 3.1416;
+// 1)
+// function calcularCirculo(radio) {
+// let circulo = radio ** 2 / Math.PI;
+// return circulo;
+// }
+// console.log(calcularCirculo(10));
+// // 2
+// function calcularRectangulo(ancho, alto) {
+//     let rectangulo = 2 * (ancho + alto);
+//     return rectangulo;
+// }
+// console.log(calcularRectangulo(10, 10))
+// // 3
+let radi = prompt("cuanto mide el radio?")
+let radionum = parseInt(radi)
+function calcularCircu(radial) {
+    
+let circul = radial ** 2 * Math.PI;
+return circul; 
+}
+console.log(calcularCircu)
